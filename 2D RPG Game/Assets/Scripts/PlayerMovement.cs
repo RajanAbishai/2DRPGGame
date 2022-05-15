d203ad46dafc43b9a3316391327392fe9d58663c
Rajan <themailboxofabi@yahoo.com>
2022-05-15 10:44:50 +0530

    Player Movement Done!

--- 2D RPG Game/Assets/Scripts/PlayerMovement.cs
+++ 2D RPG Game/Assets/Scripts/PlayerMovement.cs
@@ -19,12 +19,14 @@ public class PlayerMovement : MonoBehaviour
     private float attackTimeCounter; //counter of attacking time
 
 
-    // Start is called before the first frame update
+    
     void Start()
     {
 
         anim = GetComponent<Animator>();
         myBody = GetComponent<Rigidbody2D>();
+
+        /*Connected to the game manager*/
 
         if (!playerExists)
         {
@@ -34,14 +36,86 @@ public class PlayerMovement : MonoBehaviour
 
         else
         {
-
+            Destroy(gameObject);
         }
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        playerMoving = false;
+
+        /*If you're not attacking but you receive input along the horizontal or vertical axis, move accordingly*/
+        if (!attacking)
+        {
+            if ((Input.GetAxisRaw("Horizontal") > 0.5f ||
+                Input.GetAxisRaw("Horizontal") < -0.5f))
+            {
+                myBody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, myBody.velocity.y); //adjust only x axis
+                playerMoving = true;
+
+                lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
+            }
+
+
+
+            if ((Input.GetAxisRaw("Vertical") > 0.5f ||
+                Input.GetAxisRaw("Vertical") < -0.5f))
+            {
+                myBody.velocity = new Vector2(myBody.velocity.x, Input.GetAxisRaw("Vertical") * moveSpeed); //adjust only y axis
+                playerMoving = true;
+
+                lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
+            }
+
+        }
+
+        // Very small inputs do not count as movements for X - to prevent sliding away?
+        // range that does not allow movement: -0.5f to 0.5f
+        if ((Input.GetAxisRaw("Horizontal") < 0.5f &&
+                Input.GetAxisRaw("Horizontal") > -0.5f))
+        {
+            myBody.velocity = new Vector2(0, myBody.velocity.y);
+        }
+
+        // Very small inputs do not count as movements for Y
+        if ((Input.GetAxisRaw("Vertical") < 0.5f &&
+                Input.GetAxisRaw("Vertical") > -0.5f))
+        {
+            myBody.velocity = new Vector2(myBody.velocity.x, 0f);
+        }
+
+
+
+        if (attackTimeCounter > 0)
+        {
+            attackTimeCounter -= Time.deltaTime;
+        }
+
+        if (attackTimeCounter <= 0)
+        {
+            attacking = false;
+            anim.SetBool(TagManager.PLAYER_ATTACKING_PARAMETER,false);
+        }
+
+
+        anim.SetFloat(TagManager.PLAYER_X_MOVEMENT, Input.GetAxisRaw("Horizontal"));
+        anim.SetFloat(TagManager.PLAYER_Y_MOVEMENT, Input.GetAxisRaw("Vertical"));
+
+        anim.SetBool(TagManager.PLAYER_MOVING_PARAMETER,playerMoving); //transfer the bool value of playerMoving to player moving parameter
+
+        anim.SetFloat(TagManager.LAST_MOVE_X, lastMove.x);
+        anim.SetFloat(TagManager.LAST_MOVE_Y, lastMove.y);
+
     }
-}
+
+
+
+
+
+
+
+
+
+}  //class
---
+++ 2D RPG Game/Assets/Scripts/TagManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TagManager : MonoBehaviour
+{
+
+    public static string PLAYER_ATTACKING_PARAMETER = "PlayerAttacking";
+    public static string PLAYER_MOVING_PARAMETER = "PlayerMoving";
+
+    public static string PLAYER_X_MOVEMENT= "MoveX";
+    public static string PLAYER_Y_MOVEMENT = "MoveY";
+
+    public static string LAST_MOVE_X = "LastMoveX";
+    public static string LAST_MOVE_Y="LastMoveY";
+
+
+}
